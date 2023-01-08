5f1152aac8bc7a3af36b4f738fb7873bc2b284c2
carlooooh <carli.ilmitico@gmail.com>
2023-01-08 22:30:49 +0100

    Settaggio autentificazio del palazzo mentale con creazione dell-HUB

---
+++ POTERE/src/example_package_potere/HUB.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import *
+from tkinter import ttk
+
+
+
+def run_hub():
+
+
+    root = Tk()
+    root.title("POTERE- PALAZZO MENTALE ARTIFICIALE")
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    ttk.Button(mainframe, text="GALILEO").grid(column=3, row=3, sticky=W)
+    root.mainloop()
+    
+
+
---
+++ POTERE/src/example_package_potere/vision.py
@@ -0,0 +1,95 @@
+import os,sys
+import face_recognition
+import cv2
+import numpy as np
+
+
+
+class FaceRecognition:
+    face_loction = []
+    face_encodings= []
+    face_name=[]
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+
+    def __init__(self):
+        self.encode_faces()
+
+    
+    def encode_faces(self):
+        for image in os.listdir('./POTERE/CAMERE/GALILEO'):
+            face_image = face_recognition.load_image_file(f"./POTERE/CAMERE/GALILEO/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+    
+
+    def run_recognition(self):
+        accesso =0
+        video_capture = cv2.VideoCapture(0)
+
+        if not video_capture.isOpened():
+            sys.exit('Video source not found...')
+
+        while True:
+            ret, frame = video_capture.read()
+
+            # Only process every other frame of video to save time
+            if self.process_current_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Find all the faces and face encodings in the current frame of video
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Strunzzz"
+                    
+
+                    # Calculate the shortest distance to face
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        if name == "proprietario.jpg":
+                            accesso == 1
+                    self.face_names.append(f'{name}')
+
+            self.process_current_frame = not self.process_current_frame
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Create the frame with the name
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+            # Display the resulting image
+            cv2.imshow('POTERE', frame)
+
+            if accesso == 1:
+                exec('potere-hub.py')
+                video_capture.release()
+                cv2.destroyAllWindows()
+          
+if __name__ == '__main__':
+    fr = FaceRecognition()
+    fr.run_recognition()
