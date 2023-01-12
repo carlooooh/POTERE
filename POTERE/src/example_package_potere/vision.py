879e7c8eb869b01af72e14eb9367feb20e64602c
carlooooh <carli.ilmitico@gmail.com>
2023-01-12 15:36:49 +0100

    revisione su windows

--- POTERE/src/example_package_potere/vision.py
+++ POTERE/src/example_package_potere/vision.py
@@ -9,16 +9,20 @@ import os,sys
 import face_recognition
 import cv2
 import numpy as np
+import threading
 
 
 
-class FaceRecognition:
+
+
+class FaceRecognition2:
     face_loction = []
     face_encodings= []
     face_name=[]
     known_face_encodings = []
     known_face_names = []
     process_current_frame = True
+    
 
 
     def __init__(self):
@@ -36,18 +40,20 @@ class FaceRecognition:
     
 
     def run_recognition(self):
-        accesso =0
+        
         video_capture = cv2.VideoCapture(0)
 
         if not video_capture.isOpened():
             sys.exit('Video source not found...')
+        
 
         while True:
+            
             ret, frame = video_capture.read()
 
             
             if self.process_current_frame:
-               
+            
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
                 
@@ -70,8 +76,8 @@ class FaceRecognition:
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
                         name = self.known_face_names[best_match_index]
-                        if name == "proprietario.jpg":
-                            accesso == 1
+                        
+                            
                     self.face_names.append(f'{name}')
 
             self.process_current_frame = not self.process_current_frame
@@ -88,15 +94,30 @@ class FaceRecognition:
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
-
             
             cv2.imshow('POTERE', frame)
 
-            if accesso == 1:
-                exec('potere-hub.py')
-                video_capture.release()
-                cv2.destroyAllWindows()
-          
-if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
+            if cv2.waitKay(1) == ord('q'):
+                break
+        
+        video_capture.release()
+        cv2.destroyAllWindows()
+                
+                
+            
+def avvio():
+
+    def run():
+        fr=FaceRecognition2()
+        fr.run_recognition()
+    
+    threading.Thread(target=run).start()
+    
+    
+
+
+        
+
+
+
+
